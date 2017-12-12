d032f1bde955ae50f3d6f2f917a9028a35bb8fb2
Nefarix <oliverduarte8@gmail.com>
2017-12-12 11:12:05 -0500

    fixing mistakes

--- ActiveHouse/src/main/java/io/github/activehouse/AboutUs.java
+++ ActiveHouse/src/main/java/io/github/activehouse/AboutUs.java
@@ -15,20 +15,3 @@ public class AboutUs extends AppCompatActivity {
 
     }
 }
-
-package io.github.activehouse;
-//AHStudio
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-public class AboutUs extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about_us);
-
-
-    }
-}
-
