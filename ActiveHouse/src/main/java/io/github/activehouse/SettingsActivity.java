b89283f35ae6fde2b115e7c801ae94a90348931a
githubofryry <31857637+githubofryry@users.noreply.github.com>
2017-12-12 00:05:13 -0500

    Adding files

---
+++ ActiveHouse/src/main/java/io/github/activehouse/SettingsActivity.java
@@ -0,0 +1,16 @@
+package io.github.activehouse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/*
+*   Created by Ryan Antolin on 2017-11-12
+*
+ */
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
