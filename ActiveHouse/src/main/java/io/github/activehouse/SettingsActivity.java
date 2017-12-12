4ce41a251705bc81caa76eec5236f20e46616451
githubofryry <31857637+githubofryry@users.noreply.github.com>
2017-12-12 01:17:04 -0500

    Updating Settings
    - added ic_settings_black_24/48dp.png
    - added SettingsFragment.java
    - edited SettingsActivity.java
    - edited strings.xml, room.xml, perferences.xml, activity_settings.xml, activity_home_drawer.xml

--- ActiveHouse/src/main/java/io/github/activehouse/SettingsActivity.java
+++ ActiveHouse/src/main/java/io/github/activehouse/SettingsActivity.java
@@ -11,6 +11,9 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        getSupportFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+        //setContentView(R.layout.activity_settings);
     }
 }
