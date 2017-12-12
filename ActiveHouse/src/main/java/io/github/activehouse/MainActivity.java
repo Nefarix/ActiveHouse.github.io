027f1bb153e6612ab7e96ef4911f5c145087ffad
Nefarix <oliverduarte8@gmail.com>
2017-12-12 09:48:42 -0500

    Attempting to get Authentication Working
    Authentication Setup

--- ActiveHouse/src/main/java/io/github/activehouse/MainActivity.java
+++ ActiveHouse/src/main/java/io/github/activehouse/MainActivity.java
@@ -24,24 +24,36 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
 import java.math.BigInteger;
 import java.nio.charset.Charset;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText etUsername;
-    EditText etPassword;
+    EditText mEmailField;
+    EditText mPasswordField;
     CheckBox cbSave;
     Button btnLogin;
     String username, password;
 
-    private String TAG = MainActivity.class.getSimpleName();
+
+    private static final String TAG = "EmailPassword";
 
     public static String FirstName, LastName, Username, Email;
     public static int HouseID;
+
+    private FirebaseAuth mAuth;
 
 
 
@@ -51,14 +63,16 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        etUsername = (EditText) findViewById(R.id.input_username);
-        etPassword = (EditText) findViewById(R.id.input_password);
+        mEmailField = (EditText) findViewById(R.id.input_username);
+        mPasswordField = (EditText) findViewById(R.id.input_password);
         cbSave = (CheckBox) findViewById(R.id.input_save);
 
+        mAuth = FirebaseAuth.getInstance();
+
         SharedPreferences sharedPref = MainActivity.this.getPreferences(Context.MODE_PRIVATE);
-        String savedUsername = sharedPref.getString("username", "");
+        String savedUsername = sharedPref.getString("mUsername", "");
         if (!savedUsername.equals("")) {
-            etUsername.setText(savedUsername);
+            mEmailField.setText(savedUsername);
             cbSave.setChecked(true);
         }
 
@@ -70,18 +84,18 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
 
-                if (etPassword.getText().length() > 0 && etUsername.getText().length() > 0) {
+                if (mPasswordField.getText().length() > 0 && mEmailField.getText().length() > 0) {
 
                     //attempt login
-                    username = etUsername.getText().toString();
-                    password = etPassword.getText().toString();
+                    username = mEmailField.getText().toString();
+                    password = mPasswordField.getText().toString();
                     new GetLogin().execute();
 
 
                     if (cbSave.isChecked()) {
                         SharedPreferences sharedPref = MainActivity.this.getPreferences(Context.MODE_PRIVATE);
                         SharedPreferences.Editor editor = sharedPref.edit();
-                        editor.putString("username", etUsername.getText().toString());
+                        editor.putString("username", mEmailField.getText().toString());
                         editor.apply();
 
 
@@ -115,6 +129,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+// [END on_start_check_user]
+
+    @Override
     public void onBackPressed() {
 
             new AlertDialog.Builder(this)
@@ -135,121 +158,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private class GetLogin extends AsyncTask<Void, Void, Void> {
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            //Toast.makeText(HomeActivity.this,"Json Data is downloading",Toast.LENGTH_LONG).show();
 
-        }
-
-        @Override
-        protected Void doInBackground(Void... arg0) {
-            HttpHandler sh = new HttpHandler();
-
-            String hashedPW = md5(password);
-            // Making a request to url and getting response
-            String url = "http://munro.humber.ca/~n01046059/ActiveHouse/login.php?username=" + username + "&password=" + hashedPW;
-            String jsonStr = sh.makeServiceCall(url);
-
-            Log.e(TAG, "Response from url: " + jsonStr);
-            if (jsonStr != null) {
-                try {
-                    JSONObject jsonObj = new JSONObject(jsonStr);
-                    int success = jsonObj.getInt("success");
-                    if (success == 1) {
-                        // Getting JSON Array node
-                        JSONArray user = jsonObj.getJSONArray("user");
-
-
-                        JSONObject u = user.getJSONObject(0);
-                        HouseID = u.getInt("HOUSE_ID");
-                        Username = u.getString("USERNAME");
-                        FirstName = u.getString("FIRST_NAME");
-                        LastName = u.getString("LAST_NAME");
-                        Email = u.getString("EMAIL");
-
-                        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-                        startActivity(intent);
-
-
-                    }
-                    else {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Toast.makeText(getApplicationContext(), R.string.errorLoggingIn,Toast.LENGTH_LONG).show();
-                            }
-                        });
-                    }
-
-
-
-
-
-
-
-
-
-                } catch (final JSONException e) {
-                    Log.e(TAG, R.string.jsonError + e.getMessage());
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            Toast.makeText(getApplicationContext(),
-                                    R.string.jsonError + e.getMessage(),
-                                    Toast.LENGTH_LONG).show();
-                        }
-                    });
-
-                }
-
-            } else {
-                Log.e(TAG, "Couldn't get json from server.");
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(getApplicationContext(),
-                                R.string.jsonError3,
-                                Toast.LENGTH_LONG).show();
-                    }
-                });
-            }
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void result) {
-            super.onPostExecute(result);
-
-
-
-
-
-
-        }
-    }
-
-
-    public static String md5(String s)
-    {
-        MessageDigest digest;
-        try
-        {
-            digest = MessageDigest.getInstance("MD5");
-            digest.update(s.getBytes(Charset.forName("US-ASCII")),0,s.length());
-            byte[] magnitude = digest.digest();
-            BigInteger bi = new BigInteger(1, magnitude);
-            String hash = String.format("%0" + (magnitude.length << 1) + "x", bi);
-            return hash;
-        }
-        catch (NoSuchAlgorithmException e)
-        {
-            e.printStackTrace();
-        }
-        return "";
-    }
 
 
 }
--- ActiveHouse/src/main/java/io/github/activehouse/RegisterActivity.java
+++ ActiveHouse/src/main/java/io/github/activehouse/RegisterActivity.java
@@ -19,10 +19,22 @@ import java.nio.charset.Charset;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+
+
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class RegisterActivity extends AppCompatActivity {
 
-    EditText etUsername, etPassword, etFName, etLName, etEmail, etHouseID;
-    String Username, Password, FName, LName, Email, HouseID;
+    EditText etPassword, etEmail;
+    String Password,Email;
     Button regButton;
     private String TAG = RegisterActivity.class.getSimpleName();
 
@@ -32,12 +44,8 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        etUsername = (EditText) findViewById(R.id.input_username);
         etPassword = (EditText) findViewById(R.id.input_password);
-        etFName = (EditText) findViewById(R.id.input_fname);
-        etLName = (EditText) findViewById(R.id.input_lname);
         etEmail = (EditText) findViewById(R.id.input_email);
-        etHouseID = (EditText) findViewById(R.id.input_houseid);
         regButton = (Button) findViewById(R.id.btn_register);
 
         regButton.setOnClickListener(new View.OnClickListener() {
@@ -49,12 +57,8 @@ public class RegisterActivity extends AppCompatActivity {
                         && etLName.getText().length() > 0 && etEmail.getText().length() > 0 && etHouseID.getText().length() > 0) {
 
                     //attempt registration
-                    Username = etUsername.getText().toString();
                     Password = md5(etPassword.getText().toString());
-                    FName = etFName.getText().toString();
-                    LName = etLName.getText().toString();
                     Email = etEmail.getText().toString();
-                    HouseID = etHouseID.getText().toString();
                     new Registration().execute();
 
                 }
@@ -68,123 +72,171 @@ public class RegisterActivity extends AppCompatActivity {
 
     }
 
-
-    private class Registration extends AsyncTask<Void, Void, Void> {
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            //Toast.makeText(HomeActivity.this,"Json Data is downloading",Toast.LENGTH_LONG).show();
-
+    private void createAccount(String email, String password) {
+        Log.d(TAG, "createAccount:" + email);
+        if (!validateForm()) {
+            return;
         }
 
-        @Override
-        protected Void doInBackground(Void... arg0) {
-            HttpHandler sh = new HttpHandler();
+        showProgressDialog();
 
-            // Making a request to url and getting response
-            String url = "http://munro.humber.ca/~n01046059/ActiveHouse/register.php?username=" + Username + "&password=" + Password
-                    + "&fname=" + FName + "&lname=" + LName + "&email=" + Email + "&houseid=" + HouseID;
-            String jsonStr = sh.makeServiceCall(url);
-
-            Log.e(TAG, "Response from url: " + jsonStr);
-            if (jsonStr != null) {
-                try {
-                    JSONObject jsonObj = new JSONObject(jsonStr);
-                    int success = jsonObj.getInt("success");
-                    if (success == 1) {
-
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Toast.makeText(getApplicationContext(), R.string.regSuccess,Toast.LENGTH_LONG).show();
-                                finish();
-                            }
-                        });
-
-
-                    }
-                    else if (success == 2) {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Toast.makeText(getApplicationContext(), R.string.invalid_houseid,Toast.LENGTH_LONG).show();
-                            }
-                        });
-                    }
-                    else {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Toast.makeText(getApplicationContext(), R.string.errorRegistering,Toast.LENGTH_LONG).show();
-                            }
-                        });
-                    }
-
-
-
-
-
-
-
-
-
-                } catch (final JSONException e) {
-                    Log.e(TAG, R.string.jsonError + e.getMessage());
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            Toast.makeText(getApplicationContext(),
-                                    R.string.jsonError + e.getMessage(),
-                                    Toast.LENGTH_LONG).show();
-                        }
-                    });
-
-                }
-
-            } else {
-                Log.e(TAG, "Couldn't get json from server.");
-                runOnUiThread(new Runnable() {
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
-                    public void run() {
-                        Toast.makeText(getApplicationContext(),
-                                R.string.jsonError3,
-                                Toast.LENGTH_LONG).show();
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // [START_EXCLUDE]
+                        hideProgressDialog();
+                        // [END_EXCLUDE]
                     }
                 });
-            }
-
-            return null;
+        // [END create_user_with_email]
+    }
+    private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+        if (!validateForm()) {
+            return;
         }
 
-        @Override
-        protected void onPostExecute(Void result) {
-            super.onPostExecute(result);
+        showProgressDialog();
 
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
 
+                        // [START_EXCLUDE]
+                        if (!task.isSuccessful()) {
+                            mStatusTextView.setText(R.string.auth_failed);
+                        }
+                        hideProgressDialog();
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END sign_in_with_email]
+    }
 
+    private void signOut() {
+        mAuth.signOut();
+        updateUI(null);
+    }
 
+    private void sendEmailVerification() {
+        // Disable button
+        findViewById(R.id.verify_email_button).setEnabled(false);
 
+        // Send verification email
+        // [START send_email_verification]
+        final FirebaseUser user = mAuth.getCurrentUser();
+        user.sendEmailVerification()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // [START_EXCLUDE]
+                        // Re-enable button
+                        findViewById(R.id.verify_email_button).setEnabled(true);
 
+                        if (task.isSuccessful()) {
+                            Toast.makeText(EmailPasswordActivity.this,
+                                    "Verification email sent to " + user.getEmail(),
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            Log.e(TAG, "sendEmailVerification", task.getException());
+                            Toast.makeText(EmailPasswordActivity.this,
+                                    "Failed to send verification email.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END send_email_verification]
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = mEmailField.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            mEmailField.setError("Required.");
+            valid = false;
+        } else {
+            mEmailField.setError(null);
+        }
+
+        String password = mPasswordField.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            mPasswordField.setError("Required.");
+            valid = false;
+        } else {
+            mPasswordField.setError(null);
+        }
+
+        return valid;
+    }
+
+    private void updateUI(FirebaseUser user) {
+        hideProgressDialog();
+        if (user != null) {
+            mStatusTextView.setText(getString(R.string.emailpassword_status_fmt,
+                    user.getEmail(), user.isEmailVerified()));
+            mDetailTextView.setText(getString(R.string.firebase_status_fmt, user.getUid()));
+
+            findViewById(R.id.email_password_buttons).setVisibility(View.GONE);
+            findViewById(R.id.email_password_fields).setVisibility(View.GONE);
+            findViewById(R.id.signed_in_buttons).setVisibility(View.VISIBLE);
+
+            findViewById(R.id.verify_email_button).setEnabled(!user.isEmailVerified());
+        } else {
+            mStatusTextView.setText(R.string.signed_out);
+            mDetailTextView.setText(null);
+
+            findViewById(R.id.email_password_buttons).setVisibility(View.VISIBLE);
+            findViewById(R.id.email_password_fields).setVisibility(View.VISIBLE);
+            findViewById(R.id.signed_in_buttons).setVisibility(View.GONE);
         }
     }
 
-
-    public static String md5(String s)
-    {
-        MessageDigest digest;
-        try
-        {
-            digest = MessageDigest.getInstance("MD5");
-            digest.update(s.getBytes(Charset.forName("US-ASCII")),0,s.length());
-            byte[] magnitude = digest.digest();
-            BigInteger bi = new BigInteger(1, magnitude);
-            String hash = String.format("%0" + (magnitude.length << 1) + "x", bi);
-            return hash;
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.email_create_account_button) {
+            createAccount(mEmailField.getText().toString(), mPasswordField.getText().toString());
+        } else if (i == R.id.email_sign_in_button) {
+            signIn(mEmailField.getText().toString(), mPasswordField.getText().toString());
+        } else if (i == R.id.sign_out_button) {
+            signOut();
+        } else if (i == R.id.verify_email_button) {
+            sendEmailVerification();
         }
-        catch (NoSuchAlgorithmException e)
-        {
-            e.printStackTrace();
-        }
-        return "";
     }
 }
+
+
+
+
