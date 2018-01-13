74e8484f27e92762dc6433cbd1512a6e2cf20ccd
githubofryry <31857637+githubofryry@users.noreply.github.com>
2018-01-13 18:53:56 -0500

    Fixing Settings.
    Found out that the problem of it crashing.
    
    On line 20 in SettingsFragments, I was getting an error on the logcat regarding the xml file. I realized that you are supposed to use android.support.v7.preference.SwitchPreferenceCompat other than SwitchPreference or else it crashes. This would go with the same with ListPreferences.

--- ActiveHouse/src/main/java/io/github/activehouse/SettingsActivity.java
+++ ActiveHouse/src/main/java/io/github/activehouse/SettingsActivity.java
@@ -4,14 +4,19 @@
 
 package io.github.activehouse;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
 /*
 *   Created by Ryan Antolin on 2017-11-12
 *
  */
 public class SettingsActivity extends AppCompatActivity {
 
+    public static final String KEY_PREF_EXAMPLE_SWITCH = "example_switch";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +24,16 @@ public class SettingsActivity extends AppCompatActivity {
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
         //TODO: Figure out how I will implement the logic for the settings that will be featured.
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+
+
+
+        /* This switches to false and also test to see if it switches to all false
+        Boolean switchPref = sharedPref.getBoolean
+                (SettingsActivity.KEY_PREF_EXAMPLE_SWITCH, false);
+        Toast.makeText(this, switchPref.toString(), Toast.LENGTH_SHORT).show();
+        */
+
 
         //setContentView(R.layout.activity_settings);
     }
