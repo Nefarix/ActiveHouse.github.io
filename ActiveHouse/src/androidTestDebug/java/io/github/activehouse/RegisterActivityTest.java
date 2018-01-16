3242cabc95178f086002cf063e009555f5ae2e0c
githubofryry <31857637+githubofryry@users.noreply.github.com>
2018-01-16 11:36:34 -0500

    Changed test classes

--- ActiveHouse/src/androidTestDebug/java/io/github/activehouse/RegisterActivityTest.java
+++ ActiveHouse/src/androidTestDebug/java/io/github/activehouse/RegisterActivityTest.java
@@ -1,7 +1,10 @@
 package io.github.activehouse;
 
+import android.support.test.rule.ActivityTestRule;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -9,12 +12,19 @@ import static org.junit.Assert.*;
  * Created by Ryan on 1/16/2018.
  */
 public class RegisterActivityTest {
+    public ActivityTestRule<RegisterActivity> mActivityTestRule = new ActivityTestRule<RegisterActivity>(RegisterActivity.class);
+    private RegisterActivity mActivity = null;
     @Before
     public void setUp() throws Exception {
+        mActivity = mActivityTestRule.getActivity();
     }
+    @Test
+    public void testRegister() throws Exception{
 
+    }
     @After
     public void tearDown() throws Exception {
+        mActivity = null;
     }
 
 }
